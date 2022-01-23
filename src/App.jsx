83f302715f1879402156dc2f0064137c83fd81ef
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2022-01-23 19:22:52 +0530

    meme

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+import Header from './Components/Header/Header'
+import Meme from './Components/Meme/Meme'
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Meme/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h2>MemeGenerator</h2>
+            <h4>React-concept-project</h4>
+        </header>
+    )
+}
+
+export default Header
